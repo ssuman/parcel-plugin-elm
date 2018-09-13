5cd73c6303a60b1b45b056e76e2b03cf5b2a27e1
Christian Rocha <christian@rocha.is>
2018-09-13 12:33:42 -0400

    Remove unused module references

--- ElmAsset.js
+++ ElmAsset.js
@@ -1,7 +1,5 @@
 const elmCompiler = require('node-elm-compiler');
-const findAllDependencies = require("find-elm-dependencies").findAllDependencies;
-const fs = require('fs');
-const path = require('path');
+const { findAllDependencies } = require('find-elm-dependencies');
 const process = require('process');
 const JSAsset = require('parcel-bundler/src/assets/JSAsset');
 
@@ -18,7 +16,7 @@ class ElmAsset extends JSAsset {
     };
     return defaultOptions;
   }
-  
+
   async getDependencies() {
     await super.getDependencies()
     let deps = await findAllDependencies(this.name);
